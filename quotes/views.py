7febcbf09724cb80c54b35bebbbd73733beef9ac
Elena <lenasharabura@gmail.com>
2022-08-17 09:39:36 +0200

    edit view

--- quotes/views.py
+++ quotes/views.py
@@ -4,6 +4,6 @@ from models import Quote
 
 class QuoteListView(generic.ListView):
     model = Quote
-    queryset = Quote.objects.all()
+    queryset = Quote.objects.all().select_related('author').prefetch_related('tags')
     context_object_name = "quote_list"
     template_name = "quotes/quote_list.html"
