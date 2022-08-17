3f8d7ae8204d4bc9b85ef5d823b0289399c64a7e
Katya Vasylenko <kettyblack20@gmail.com>
2022-08-17 10:15:26 +0300

    models created

--- quotes/models.py
+++ quotes/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    full_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.full_name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=63)
+
+    def __str__(self):
+        return self.name
+
+
+class Quote(models.Model):
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, related_name="quotes")
+
+    def __str__(self):
+        return self.content
